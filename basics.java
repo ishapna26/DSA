d8d73d678015785c6ebf066088d7eb883a0718c4
SHAPNA <160203414+ishapna26@users.noreply.github.com>
2025-05-18 22:55:25 +0530

    Update basics.java

--- basics.java
+++ basics.java
@@ -1,1 +1,17 @@
+/*Prob 1 : Write a program that takes an integer,
+then a string, then a char from the user and prints them in the screen.*/
 
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int num = sc.nextInt();
+        String str = sc.next();
+        char ch = sc.next().charAt(0);
+
+        System.out.println(num);
+        System.out.println(str);
+        System.out.println(ch);
+    }
+}
