49dd34fd629144885cde0cb7f196a7f89487e97a
SHAPNA <160203414+ishapna26@users.noreply.github.com>
2025-05-25 15:54:53 +0530

    Update Tpattern2.c

--- Tpattern2.c
+++ Tpattern2.c
@@ -1,1 +1,43 @@
+p.  * * * * *
+     * * * *
+      * * *
+       * *
+        *
+        *
+       * *
+      * * *
+     * * * *
+    * * * * *
 
+#include<stdio.h>
+void main(){
+    int n;
+    scanf("%d",&n);
+    
+    for(int i=1;i<=2*n;i++){
+        if(i>n){
+            for(int k=1;k<=(2*n)-i;k++){
+                printf(" ");
+            }
+            for(int j=1;j<=((2*i)-(2*n))-1;j++){
+                printf("*");
+            }
+            for(int k=1;k<=(2*n)-i;k++){
+                printf(" ");
+            }
+            printf("\n");
+        }
+        else{
+        for(int k=1;k<=i-1;k++){
+            printf(" ");
+        }
+        for(int j=1;j<=2*(n-i)+1;j++){
+            printf("*");
+        }
+        for(int k=1;k<=i-1;k++){
+            printf(" ");
+        }
+        printf("\n");
+        }
+    }
+}
