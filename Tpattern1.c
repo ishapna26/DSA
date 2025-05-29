479a918d27f4fa723d36bec47372850fb18a1213
SHAPNA <160203414+ishapna26@users.noreply.github.com>
2025-05-29 22:33:04 +0530

    Update Tpattern1.c

--- Tpattern1.c
+++ Tpattern1.c
@@ -428,6 +428,33 @@ void main(){
     }
 }
 
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
+
+import java.util.*;
+class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n=sc.nextInt();
+
+        for(int i=1;i<=(2*n)-1;i++){
+            int rowChange=(i>n)?(2*n)-i:i;
+            for (int k=1;k<=n-rowChange;k++){
+                System.out.print(" ");
+            }
+            for (int j=1;j<=(2*rowChange)-1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
 
 
 
