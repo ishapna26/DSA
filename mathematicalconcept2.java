e9be6b8a72c564718f8ce50b8ce80966c37a6bd4
SHAPNA <160203414+ishapna26@users.noreply.github.com>
2025-06-06 20:53:37 +0530

    Update mathematicalconcept2.java

--- mathematicalconcept2.java
+++ mathematicalconcept2.java
@@ -107,3 +107,70 @@ class Main{
         return (factorial);
     }
 }
+
+//Prime Numbers
+4. Write a program that gets n as input and print all the prime numbers till that number.
+Input : 5
+Expected output:
+2 3 5
+
+import java.util.*;
+
+class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int num=2;num<=n;num++){
+            int count=0;
+            for(int j=2;j<=num;j++){
+                if(num%j==0){
+                    count++;
+                }
+            }
+            if(count<2){
+                System.out.print(num+" ");
+            }
+        }
+
+    }
+}
+
+NOTE : TO check if a number is palindrome or not run only upto root(n) because after root n the factors pairs just reverse like n=4 (1*4, 2*2, 4*1)
+class Solution {
+    static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false; // Not a prime
+            }
+        }
+        return true; // Prime
+    }
+}
+
+5. Given an integer n, find the number of divisors of n that are divisible by 3.
+import java.util.*;
+class Main{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        int count =0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                if(i%3==0){
+                    count++;
+                }
+            }
+        }
+        System.out.print(count);
+    }
+}
+
+
+
+
+    
+
