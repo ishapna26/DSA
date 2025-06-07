02588f60016e274d9a4611248fed16a1bc2ed8aa
SHAPNA <160203414+ishapna26@users.noreply.github.com>
2025-06-07 14:31:24 +0530

    Update Tpattern2.c

--- Tpattern2.c
+++ Tpattern2.c
@@ -9,39 +9,28 @@ p.  * * * * *
      * * * *
     * * * * *
 
-#include<stdio.h>
-void main(){
-    int n;
-    scanf("%d",&n);
-    
-    for(int i=1;i<=2*n;i++){
-        if(i>n){
-            for(int k=1;k<=(2*n)-i;k++){
-                printf(" ");
-            }
-            for(int j=1;j<=((2*i)-(2*n))-1;j++){
-                printf("*");
-            }
-            for(int k=1;k<=(2*n)-i;k++){
-                printf(" ");
-            }
-            printf("\n");
-        }
-        else{
-        for(int k=1;k<=i-1;k++){
-            printf(" ");
-        }
-        for(int j=1;j<=2*(n-i)+1;j++){
-            printf("*");
-        }
-        for(int k=1;k<=i-1;k++){
-            printf(" ");
 
+import java.util.*;
+public class Main {
 
-        }
-        printf("\n");
-        }
-    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		
+		for(int i=1;i<=2*n;i++) {
+			int row=(i>n)?(2*n)-i+1:i;
+			for(int k=1;k<=row-1;k++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=n-row+1;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+
+	}
+
 }
 
 
