413fd1c6c9abfa62a24b397ca26aa85e0a79a363
SHAPNA <160203414+ishapna26@users.noreply.github.com>
2025-06-09 00:43:19 +0530

    Update array.java

--- array.java
+++ array.java
@@ -1,1 +1,14 @@
+INTRODUCTION TO DSA:
+Data: Data is an important part of the program
+Program: A program is a set of instructions which performs operation on data.
+Data Structure: The way you organize data in main memory during execution time of a program. (Two types: Physical DS and Logical DS) / structured way to store data. 
+                                                                                                             |             |
+                                                 (How the data is arranged in the memory- Arrays, Matrices, Linked List)   (How the data can be arranged and utilized- Stack,Queues, Graphs, Trees, Hashing).
 
+Recursion usually felt inefficient because it uses stacK DS internally, (not useful for solving larger problems)
+
+INTRODUCTION TO ARRAYS
+Arrays: Arrays are defined as a collection of "similar" data items stored "contiguously" in the memory blocks.
+in c: int arr[10]; -> declaration int arr[3]={0,1,2} -> Declaration and Initialization;
+
+Why we store same DataType in a Array?
