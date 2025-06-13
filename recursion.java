17eb5396ee6918562212d813aaeee905ee383b84
SHAPNA <160203414+ishapna26@users.noreply.github.com>
2025-06-13 23:17:08 +0530

    Update recursion.java

--- recursion.java
+++ recursion.java
@@ -23,4 +23,69 @@ HOW TO SOLVE ANY RECURSIVE PROBLEMS:
 4. Find base condition.
 5. Build recursive tree.
 
-  
+-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+PROBLEMS
+1. Print numbers from 1 to n without the help of loops
+Input: n = 10
+Output: 1 2 3 4 5 6 7 8 9 10
+
+class Solution {
+    public void printNos(int n) {
+        // Code here
+        if(n==0){
+            return;
+        }
+        printNos(n-1);
+        System.out.print(n+" "); //gets executed at returning time;
+    }
+}
+
+2. The Fibonacci sequence is defined as follows: 0 1 1 2 3 5 8 ...
+F(0) = 0
+F(1) = 1
+F(n) = F(n - 1) + F(n - 2) for n > 1
+
+// User function Template for Java
+
+class Solution {
+    public int nthFibonacci(int n) {
+        // code here
+        if(n==0){
+            return 0;
+        }
+        else if(n==1){
+            return 1;
+        }
+        return nthFibonacci(n-1)+nthFibonacci(n-2);
+    }
+}
+
+3. Count the number of zeroes in a number
+import java.util.*;
+
+class Main{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		System.out.println(countz(n));
+	}
+	public static int countz(int n) {
+		if(n%10==n) {
+			if(n%10==0) {
+				return 1;
+			}
+			else {
+				return 0;
+			}
+		}
+		if(n%10==0) {
+			return 1+countz(n/10);
+		}
+		else {
+			return 0+countz(n/10);
+		}
+	}
+}
+
+
+
