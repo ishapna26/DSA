5fb38023d1d1891e70d0c8bed1b0765946867272
SHAPNA <160203414+ishapna26@users.noreply.github.com>
2025-06-19 01:38:35 +0530

    Update SortingTechniques.java

--- SortingTechniques.java
+++ SortingTechniques.java
@@ -35,7 +35,68 @@ class Main{
 --------------
 An array is divided into n sublists. These sublists are then merged to produce sorted list.
 
+import java.util.*;
 
+class Main2{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int n=sc.nextInt();
+		int a[] =new int[n];
+		int b[] =new int[n];
+		int lb=0,ub=n-1;
+		for(int i=0;i<n;i++) {
+			a[i]=sc.nextInt();
+		}
+		mergeSort(a,b,lb,ub);
+		
+		for(int i=0;i<n;i++) {
+			System.out.print(a[i]+" ");
+		}
+		sc.close();
+	}
+	public static void mergeSort(int[] a, int[] b, int lb, int ub) {
+		if(lb<ub) {
+			int mid=(lb+ub)/2;
+			mergeSort(a,b,lb,mid);
+			mergeSort(a,b,mid+1,ub);
+			merge(a,b,lb,mid,ub);
+			
+			for(int i=lb;i<=ub;i++) {
+				a[i]=b[i];
+			}
+		}
+	}
+	public static void merge(int[] a, int b[] , int lb, int mid, int ub) {
+		int i=lb,j=mid+1,k=i;
+		while(i<=mid && j<=ub) {
+			if(a[i]<=a[j]) {
+				b[k]=a[i];
+				i++;
+				k++;
+			}
+			else if(a[j]<=a[i]) {
+				b[k]=a[j];
+				j++;
+				k++;
+			}
+		}
+		if(j>ub) {
+			while(i<=mid) {
+				b[k]=a[i];
+				i++;
+				k++;
+			}
+		}
+		else {
+			while(j<=ub) {
+				b[k]=a[j];
+				j++;
+				k++;
+			}
+		}
+	}
+}
 
 
 
