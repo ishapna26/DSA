db9ecc63f95e3ee95166381065761a1ce6a45525
SHAPNA <160203414+ishapna26@users.noreply.github.com>
2025-06-20 01:35:08 +0530

    Update arraylogicbuilding.java

--- arraylogicbuilding.java
+++ arraylogicbuilding.java
@@ -115,3 +115,30 @@ public class Solution {
     }
 }
 
+5. Rotate Array: formula [ a[i]= a[i-1] ] whre i=n-1;
+import java.util.*;
+
+class Main2{
+	public static void main(String[] args) {
+		
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+        int[] a=new int[n];
+        
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        
+        int temp=a[n-1];
+        
+        for(int i=n-1;i>=1;i--) {
+        	a[i]=a[i-1];
+        }
+        a[0]=temp;
+        for(int i=0;i<n;i++) {
+        	System.out.print(a[i]+" ");
+        }
+    }
+}
+	
