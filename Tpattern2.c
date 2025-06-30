39bcef47782df125c12df8408766fe5bdac79562
SHAPNA <160203414+ishapna26@users.noreply.github.com>
2025-07-01 01:19:40 +0530

    Update Tpattern2.c

--- Tpattern2.c
+++ Tpattern2.c
@@ -40,36 +40,29 @@ q.       *
       *     *
      *********
 
-#include<stdio.h>
+import java.util.*;
 
-void main(){
-    int n;
-    scanf("%d",&n);
-    
-    for(int i=1;i<=n;i++){
-        if(i==n){
-            for(int j=1;j<=(2*n)-1;j++){
-                printf("*");
-            }
-        }
-        else{
-            for(int j=1;j<=n-i;j++){
-                printf(" ");
-            }
-            for(int k=1;k<=(2*i)-1;k++){
-                if((k==1)||k==((2*i)-1)){
-                    printf("*");
-                }
-                else{
-                    printf(" ");
-                }
-            }
-        }
-        printf("\n");
-    }
+class Main3{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		
+		for(int i=1;i<=n;i++) {
+			for(int k=1;k<=n-i;k++) {
+				System.out.print(" ");
+			}
+			for(int j=1;j<=2*i-1;j++) {
+				if(i==n||j==1||j==2*i-1) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	}
 }
-
-
 
 r.   *********
       *     *
