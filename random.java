3c93792c23bccbf0d3f197eec84dbc39b93cd1dd
SHAPNA <160203414+ishapna26@users.noreply.github.com>
2025-07-31 22:38:10 +0530

    Update random.java

--- random.java
+++ random.java
@@ -75,3 +75,36 @@ class Main9{
 		}
 	}
 }
+
+2. Zoho Pattern
+import java.util.*;
+
+class Main9{
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		String str=sc.nextLine();
+		
+		int count=1;
+		String res="";
+		char ch=0;
+		for(int i=0;i<str.length()-1;i++) {
+			ch=str.charAt(i);
+			
+			if(ch!=str.charAt(i+1)) {
+				if(count>1) {
+				res+=count+""+ch;
+				count=1;
+				}
+			}
+			else {
+				count++;
+				continue;
+			}
+		}
+		if(count>1) {
+			res+=count+""+ch;
+		}
+		System.out.print(res);
+	}
+}
