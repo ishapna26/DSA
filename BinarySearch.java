8e6072fc9339becf547f327d03ceea6f7547cc6f
SHAPNA <160203414+ishapna26@users.noreply.github.com>
2025-08-14 13:27:48 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,1 +1,8 @@
+Binary Search:
+--------------
+1. Binary Search is searching technique performed on a sorted array only.
+2. It is a very efficient searching technique.
+3. It can be implemented using iteration and recursion.
+   ie. Iterative Binary Search
+       Recursive Binary Search
 
