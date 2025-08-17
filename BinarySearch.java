4603a1a38e85d47624b1603d95178cf9c04f2619
SHAPNA <160203414+ishapna26@users.noreply.github.com>
2025-08-17 19:14:10 +0530

    Update BinarySearch.java

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,4 +1,4 @@
-Binary Search:
+Binary Search: Divide and conquer technique
 --------------
 1. Binary Search is searching technique performed on a sorted array only.
 2. It is a very efficient searching technique.
@@ -6,3 +6,41 @@ Binary Search:
    ie. Iterative Binary Search
        Recursive Binary Search
 
+4. It divides the array into n subarray and performs the search.
+
+code:
+import java.util.*;
+
+class Main9{
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int n=sc.nextInt();
+		int[] a=new int[n];
+		int index=-1;
+		for(int i=0;i<n;i++) {
+			a[i]=sc.nextInt();
+		}
+		int key=sc.nextInt();
+		Arrays.sort(a);
+		
+		int l=0;
+		int r=n-1;
+		
+		while(l<=r) {
+			int mid=(l+r)/2;
+			if(a[mid]==key) {
+				index=mid;
+				break;
+			}
+			if(key<a[mid]) {
+				r=mid;
+			}
+			if(key>a[mid]) {
+				l=mid+1;
+			}
+		}
+		System.out.print(index);
+		
+	}
+}
