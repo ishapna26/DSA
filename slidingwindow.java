23e370ce0fe8660f27ddecce1f7255aa290c3d4c
SHAPNA <160203414+ishapna26@users.noreply.github.com>
2025-09-19 15:46:07 +0530

    Update slidingwindow.java

--- slidingwindow.java
+++ slidingwindow.java
@@ -1,1 +1,4 @@
-
+Sliding window:
+It is an algorithm which makes use of the computed values to perform next computation.
+Code Reusability in an efficient manner
+Mainly used for subarrays and substring problems
